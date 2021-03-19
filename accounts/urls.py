01d688d73cbb84826bce2befb47c76f164b619aa
川野晴也 <haru@kawanoharuyanoMacBook-Air.local>
2021-03-19 15:09:20 +0900

    heroku

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('logout/', views.LogoutView.as_view(), name='account_logout'),  # ログアウトページにアクセスがあった場合にviews.pyのLogoutViewを開く
     path('signup/', views.SignupView.as_view(), name='account_signup'),  # サインアップページにアクセスがあった場合にviews.pyのSignupViewを開く
     path('profile/', views.ProfileView.as_view(), name='profile'),  # プロフィールページにアクセスがあった場合にviews.pyのProfileViewを開く
+    path('profile/edit/', views.ProfileEditView.as_view(), name='profile_edit'),  # プロフィールページにアクセスがあった場合にviews.pyのProfileViewを開く
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from allauth.account import views  # allauthaccountのviewsをimport
 from django.views import View
+from accounts.models import CustomUser
+from accounts.forms import ProfileForm
 
 
 # LoginViewクラスを作成します。引数にallauthのLoginViewを継承させます。
@@ -20,4 +22,40 @@ class SignupView(views.SignupView):
 
 class ProfileView(View):
     def get(self, request, *args, **kwargs):
-        return render(request, 'accounts/profile.html')
+        user_data = CustomUser.objects.get(id=request.user.id)
+
+        return render(request, 'accounts/profile.html', {
+            'user_data': user_data,
+        })
+
+
+
+class ProfileEditView(View):
+    def get(self, request, *args, **kwargs):
+        user_data = CustomUser.objects.get(id=request.user.id)
+        form = ProfileForm(
+            request.POST or None,
+            initial = {
+                'first_name': user_data.first_name,
+                'last_name': user_data.last_name,
+                'department': user_data.department,
+            }
+        )
+
+        return render(request, 'accounts/profile_edit.html', {
+            'form': form
+        })
+    
+    def post(self, request, *args, **kwargs):
+        form = ProfileForm(request.POST or None)
+        if form.is_valid():
+            user_data = CustomUser.objects.get(id=request.user.id)
+            user_data.first_name = form.cleaned_data['first_name']
+            user_data.last_name = form.cleaned_data['last_name']
+            user_data.department = form.cleaned_data['department']
+            user_data.save()  # 入力データをデータベースに保存する
+            return redirect('profile')  # 編集が完了したらプロフィール画面に遷移
+        
+        return render(request, 'accounts/profile.html', {
+            'form': form
+        })
