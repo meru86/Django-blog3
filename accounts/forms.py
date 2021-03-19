1106d6f16c3166168b9ea870487aa461feb997dd
川野晴也 <haru@kawanoharuyanoMacBook-Air.local>
2021-03-19 17:43:24 +0900

    heroku

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,7 +1,8 @@
 from django import forms
 
 
-class ProfileForm(forms.From):
+
+class ProfileForm(forms.Form):
     first_name = forms.CharField(max_length=30, label='姓')
     last_name = forms.CharField(max_length=30, label='名')
     department = forms.CharField(max_length=30, label='所属', required=False)
